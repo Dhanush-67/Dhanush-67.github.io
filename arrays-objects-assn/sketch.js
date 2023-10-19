b3bd75b094c606aacd2e455acaa824b86ed924f1
Dhanush-67 <146897841+Dhanush-67@users.noreply.github.com>
2023-10-19 11:16:18 -0600

    Worked on making a tracker function for the turrets

--- arrays-objects-assn/sketch.js
+++ arrays-objects-assn/sketch.js
@@ -27,6 +27,7 @@ let ufoDx = 0;
 let ufoDy = 0;
 let ufoX;
 let ufoY;
+let ufoHeading;
 
 
 function preload(){
@@ -43,93 +44,98 @@ function setup() {
   createCanvas(screenWidth, screenHeight);
   let xcor = [0,screenWidth/2,screenWidth-80,screenWidth/2];
   let ycor = [screenHeight/2,0,screenHeight/2,screenHeight-80];
+
   for(let i = 0; i < 4; i++){
     let theTurret = spawnTurret(xcor[i],ycor[i]);
     turrets.push(theTurret);
   }
+
   ufoPos = createVector(screenWidth/2, screenHeight/2);
+  ufoHeading = ufoPos.heading();
 }
 
 function draw() {
   if (state === "start game"){
-    startGameScreen()
+    startGameScreen();
   }
 
   if (state === "game"){
-  background(backgroundImg);
-  displayBoundary();
-  displayTurret();
-  displayUfo();
-  moveUfo();
-  displayScore();
+    background(backgroundImg);
+    displayBoundary();
+    displayTurret();
+    displayUfo();
+    moveUfo();
+    displayScore();
   }
 }
 
 function startGameScreen(){
-  push()
-  background(backgroundImg)
-  imageMode(CENTER)
-  image(playButton,screenWidth/2,screenHeight/2,playButton.width*playButtonSize,playButton.height*playButtonSize)
+  push();
+  background(backgroundImg);
+  imageMode(CENTER);
+  image(playButton,screenWidth/2,screenHeight/2,playButton.width*playButtonSize,playButton.height*playButtonSize);
   if (mouseIsPressed === true && state === "start game"){
-    state = "game"
+    state = "game";
   }
-  pop()
+  pop();
 }
 
 function displayScore(){
-  push()
-  textSize(50)
-  textStyle("BOLD")
-  text("Score:",50,50)
-  pop()
+  push();
+  textSize(50);
+  textStyle("BOLD");
+  text("Score:",50,50);
+  pop();
 }
 
 function displayUfo(){
-  push()
+  push();
   imageMode(CENTER);
-  ufoPos.x = constrain(ufoPos.x,115,1165)
-  ufoPos.y = constrain(ufoPos.y,105,610)
-  image(ufoImg,ufoPos.x,ufoPos.y,ufoImg.width*ufoSize,ufoImg.height*ufoSize)
-  pop()
+  ufoPos.x = constrain(ufoPos.x,115,1165);
+  ufoPos.y = constrain(ufoPos.y,105,610);
+  image(ufoImg,ufoPos.x,ufoPos.y,ufoImg.width*ufoSize,ufoImg.height*ufoSize);
+  pop();
 }
 
 function moveUfo(){
-    ufoPos.x +=  ufoDx
-    ufoPos.y += ufoDy
+  ufoPos.x +=  ufoDx;
+  ufoPos.y += ufoDy;
 }
 
 
 function keyPressed() {
-   if (keyCode === 68) {
+  if (keyCode === 68) {
     ufoDx = 4;
     ufoDy = 0;
-   } else if (keyCode === 65) {
-     ufoDx = -4;
-     ufoDy = 0;
-   } else if (keyCode === 87) {
-     ufoDy = -4;
-     ufoDx = 0;
-   }
-   else if (keyCode === 83){
-     ufoDy = 4;
-     ufoDx = 0;
-   }
+  }
+  else if (keyCode === 65) {
+    ufoDx = -4;
+    ufoDy = 0;
+  } 
+  else if (keyCode === 87) {
+    ufoDy = -4;
+    ufoDx = 0;
+  }
+  else if (keyCode === 83){
+    ufoDy = 4;
+    ufoDx = 0;
+  }
 }
 
 
 function displayBoundary(){
-  push()
+  push();
   stroke("blue");
   strokeWeight(4);
   let l1 = createVector(80,80);
   let l2 = createVector(80,640);
   let l3 = createVector(1200,80);
   let l4 = createVector(1200,640);
-  wall1 = line(l1.x,l1.y,l2.x,l2.y);
-  wall2 = line(l1.x,l1.y,l3.x,l3.y);
-  wall3 = line(l2.x,l2.y,l4.x,l4.y);
-  wall4 = line(l3.x,l3.y,l4.x,l4.y);
-  pop()
+  let wall1 = line(l1.x,l1.y,l2.x,l2.y);
+  let wall2 = line(l1.x,l1.y,l3.x,l3.y);
+  let wall3 = line(l2.x,l2.y,l4.x,l4.y);
+  let wall4 = line(l3.x,l3.y,l4.x,l4.y);
+  pop();
 }
 
 function spawnTurret(xpos,ypos){
@@ -145,6 +151,10 @@ function displayTurret(){
   let turretImg = [turretImgRight,turretImgDown,turretImgLeft,turretImgUp];
   for(let i = 0; i <turrets.length; i++ ){
     let theTurret = turrets[i];
+    push();
+    line(theTurret.x, theTurret.y, ufoPos.x, ufoPos.y);
+    rotate(ufoHeading);
+    pop();
     image(turretImg[i],theTurret.x,theTurret.y,turretImg[i].width * 0.10, turretImg[i].height * 0.10);
   }
 }
