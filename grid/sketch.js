6394062d50fd99c7b5b68cf01d78a39d0158d0f0
Dhanush-67 <7003902@spsd.sk.ca>
2023-10-24 23:55:05 -0600

    Updated the grid demo

--- grid/sketch.js
+++ grid/sketch.js
@@ -16,51 +16,68 @@ const GRID_SIZE = 15;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  if (height > width){
-    cellSize = width/GRID_SIZE
-  }
-  else{
-    cellSize = height/GRID_SIZE
-  }
 
-  grid = generateRandomGrid(GRID_SIZE)
-
+  if (height > width) {
+    cellSize = width/GRID_SIZE;
+  }
+  else {
+    cellSize = height/GRID_SIZE;
+  }
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
 }
 
 function draw() {
   background(220);
   displayGrid();
-
 }
 
-function keyTyped(){
-  if (key === "r"){
-    grid = generateRandomGrid(Grid_SIZW)
+function keyTyped() {
+  if (key === "r") {
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+  else if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
   }
 }
 
-function displayGrid(){
-  for (let y = 0; y<4; y++){
-    for (let x = 0; x <4; x++){
-      rect(x*cellSize, y*cellSize, cellSize,cellSize);{
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
     }
   }
 }
-}
 
-function generateRandomGrid(cols,row){
+
+function generateRandomGrid(cols, rows) {
   let randomArray = [];
-  for(let y = 0; y < cols; y++){
+  for (let y = 0; y < cols; y++) {
     randomArray.push([]);
-    for (let x = 0; y <rows; x++){
-      (for x in 90)
-      if (random(100)<50){
-        randomArray[y].push(0)
+    for (let x = 0; x < rows; x++) {
+      if (random(100) < 50) {
+        randomArray[y].push(0);
       }
-      else{
+      else {
         randomArray[y].push(1);
       }
     }
   }
-  return Array
+  return randomArray;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let randomArray = [];
+  for (let y = 0; y < cols; y++) {
+    randomArray.push([]);
+    for (let x = 0; x < rows; x++) {
+      randomArray[y].push(0);
+    }
+  }
+  return randomArray;
 }
