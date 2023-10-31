480b3aed74dcb32b1aad5e9dbd4097a04d691112
Dhanush-67 <146897841+Dhanush-67@users.noreply.github.com>
2023-10-31 11:45:23 -0600

    Started working on grid assignment

--- grid-assignment/sketch.js
+++ grid-assignment/sketch.js
@@ -6,13 +6,41 @@
 // - describe what you did to take this project "above and beyond"
 
 //global variables
-let gridSize;
+let cellSize;
 let grid;
+const GRID_SIZE = 4;
 
 function setup() {
+
   createCanvas(windowWidth, windowHeight);
+
+  if (height > width){
+    cellSize = width/GRID_SIZE;
+  }
+  else{
+    cellSize = height/GRID_SIZE;
+  }
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
 }
 
 function draw() {
   background(220);
+  //displayGrid();
+}
+
+function generateRandomGrid(cols, rows) {
+  let randomArray = [];
+  for (let y = 0; y < cols; y++) {
+    randomArray.push([]);
+    for (let x = 0; x < rows; x++) {
+      if (random(100) < 50) {
+        randomArray[y].push(0);
+      }
+      else {
+        randomArray[y].push(1);
+      }
+    }
+  }
+  console.log(randomArray);
+  return randomArray;
 }
