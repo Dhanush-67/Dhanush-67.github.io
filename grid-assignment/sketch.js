66f05ba87946dd0fcb7154be11074cf71e049515
Dhanush-67 <146897841+Dhanush-67@users.noreply.github.com>
2023-11-01 12:17:54 -0600

    worked on grid assignment

--- grid-assignment/sketch.js
+++ grid-assignment/sketch.js
@@ -8,24 +8,40 @@
 //global variables
 let cellSize;
 let grid;
-const GRID_SIZE = 4;
+const GRID_SIZE = 20;
 
 function setup() {
 
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth*0.95, windowHeight*0.95);
 
   if (height > width){
+    //cellSize = 10;
     cellSize = width/GRID_SIZE;
   }
   else{
+    //cellSize = 10;
     cellSize = height/GRID_SIZE;
   }
   grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
 }
 
 function draw() {
-  background(220);
-  //displayGrid();
+  background(255);
+  displayGrid();
+}
+
+function displayGrid(){
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (grid[y][x] === 0) {
+        fill("blue");
+      }
+      if (grid[y][x] === 1) {
+        fill("yellow");
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
 }
 
 function generateRandomGrid(cols, rows) {
